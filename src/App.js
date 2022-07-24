f33ce731fbe67bcf918f3911a1460397490f0875
Robert Gripshover <robertgrips@gmail.com>
2022-07-24 11:04:04 -0400

    added the rest of the app js file

--- src/App.js
+++ src/App.js
@@ -37,18 +37,79 @@ const startingList = [
   }
 ]
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+const Section = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  font-family: 'Kdam Thmor Pro', sans-serif;
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+`;
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+const H1 = styled.section`
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+  font-family: 'Kdam Thmor Pro', sans-serif;
+  font-size: xx-large;
+`;
+
+const H2 = styled.section`
+  font-family: 'Kdam Thmor Pro', sans-serif;
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+`;
+
+const Footer = styled.section`
+  font-family: 'Kdam Thmor Pro', sans-serif;
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+`;
+
+const App = () => {
+
+  const [myVariable, setMyVariable] = useState('Robert Gripshover')
+  const [characterList, setCharacterList] = useState(startingList)
+
+  console.log('It looks like this is working', myVariable)
+
+  useEffect(() => {
+    setMyVariable('6 Great Characters!')
+    console.log('this is sparta')
+
+    axios.get('https://swapi.dev/api/people/')
+      .then((res) => {
+        console.log(res.data)
+        setCharacterList(res.data)
+      })
+      
+  }, [])
 
   return (
+    
+     /*MAKE SURE STUFF DOES NOT LEAVE THIS DIV*/
     <div className="App">
-      <h1 className="Header">Characters</h1>
+
+      <header>
+
+      <H1 className="Header">Character List Star Wars</H1>
+      <H2>{myVariable}</H2>
+
+      </header>
+      <main>
+        <Section>
+          {
+            characterList.map((person, index) => (
+              <Character jedi={person} key={`${person.name}-${person.index}`}/>
+            )
+            )
+}   
+        </Section>
+      </main>
+
+<Footer>Robert Gripshover 2022</Footer>
     </div>
+    //MAKE SURE STUFF DOES NOT LEAVE THIS DIV
+    
   );
 }
 
