378c30196fac74b52b942d4119bf39188e3ba939
Robert Gripshover <robertgrips@gmail.com>
2022-07-24 11:06:03 -0400

    added the Character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+
+const Character = (proppy) => {
+
+    console.log(proppy);
+
+    console.log(proppy.jedi.name)
+    
+    console.log(proppy.jedi.url)
+
+    const {jedi} = proppy
+
+    console.log(jedi)
+
+    const {name, url} = jedi
+
+    console.log(name)
+
+
+return (
+    <article>
+        <h2>{name}</h2>
+        <p>{`There is more info about them at ${url}`}</p>
+    </article>
+)
+
+}
+
+export default Character
